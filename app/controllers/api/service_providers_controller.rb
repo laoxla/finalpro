465d3466c0be33a3824f6d9c7f560e5a6268df2a
olaoluwa siyanbola <ola@olaoluwas-MacBook-Pro.local>
2015-11-09 20:07:55 -0600

    corrected authetication problem

--- app/controllers/api/service_providers_controller.rb
+++ app/controllers/api/service_providers_controller.rb
@@ -1,5 +1,7 @@
 class Api::ServiceProvidersController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!
+
 
   def index
     @user = User.where("id = ?", @current_user.id)
